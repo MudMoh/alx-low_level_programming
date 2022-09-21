fdff7da5be0c92ac5bf4dfbad82f9fb7c90968b8
MudMoh <mudesir@gmail.com>
2022-09-21 11:12:59 -0700

    C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = '"';
+	printf("and that piece of art is useful" ,ch, " - Dora Korpar, 2015-10-19");
+	return (1);
+
+}
